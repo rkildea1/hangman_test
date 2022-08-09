c8a4e006860b73182461842167cce6934120a864
rkildea1 <31891933+rkildea1@users.noreply.github.com>
2022-08-09 18:34:49 +0100

    adding topic 1

--- hangman/hangman_Solution.py
+++ hangman/hangman_Solution.py
@@ -73,6 +73,12 @@ class Hangman:
         2. If the character is a single character
         If it passes both checks, it calls the check_letter method.
         '''
+        while True:
+            letter = input('Please enter letter: ')
+            if len(letter) >1:
+                print("Please, enter just one character")
+            
+            
         # TODO 1: Ask the user for a letter iteratively until the user enters a valid letter
         # TODO 1: Assign the letter to a variable called `letter`
         # TODO 1: The letter has to comply with the following criteria: It has to be a single character. If it is not, print "Please, enter just one character"
@@ -83,6 +89,7 @@ class Hangman:
 def play_game(word_list):
     # As an aid, part of the code is already provided:
     game = Hangman(word_list, num_lives=5)
+    game.ask_letter()
     # TODO 1: To test this task, you can call the ask_letter method
     # TODO 2: To test this task, upon initialization, two messages should be printed 
     # TODO 3: To test this task, you call the ask_letter method and check if the letter is in the word
