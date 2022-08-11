a7977374f23c96edc394a1a313fa37c1234da0fd
rkildea1 <31891933+rkildea1@users.noreply.github.com>
2022-08-11 10:21:37 +0100

    adding feature to handle upper and lower case

---
+++ hangman/hangman_skeleton_template.py
@@ -0,0 +1,19 @@
+import random
+class Hangman:
+    
+    def __init__(self, word_list, num_lives=5):
+        pass
+    
+    def check_letter(self, letter):
+        pass
+
+    def ask_letter(self):
+        pass
+
+def play_game():
+    pass
+
+
+if __name__ == '__main__':
+    word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry', 'watermelon']
+    play_game(word_list)
