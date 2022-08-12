57e499a8ab15c67a435ca9247204f64e97472aa3
Ivan Ying Xuan <58112372+IvanYingX@users.noreply.github.com>
2022-08-12 12:53:21 +0200

    Corrections

--- hangman/hangman_solution.py
+++ hangman/hangman_solution.py
@@ -16,10 +16,13 @@ class Hangman:
 
         print(f"The mystery word has {len(self.word)} characters") #print how long the word is
         print(f'{self.word_guessed}') #print the word in underscores eg, ['_ _ _ _']
+
+        # TODO This pass can be removed
         pass
 
 
     def check_letter(self, letter):
+        # TODO Remove these two variables (alphabet and alphabetList). You are not using them
         alphabet = list('abcdefghijklmnopqrstuvwxyz')
         alphabetList = []
         for x,y in enumerate(self.word): # x = the index of the string, y = the corresponding letter of the hangmanword
@@ -34,12 +37,15 @@ class Hangman:
              
 
     def ask_letter(self):
-
+            # TODO This condition should be inside the "play_game" function.
+            # TODO That way, this method only asks once for an input, which is what the name of the method suggests
             while self.num_lives>0 and '_' in self.word_guessed:
                 print(f'you have {self.num_lives} lives left')
                 letter = input('Please enter a letter: ')
                 letter = letter.lower()
                 if len(letter) !=1:
+                        # TODO If the letter is not valid, you don't need to decrease the n_lives
+                        # TODO (You can if you want to make the game harder)
                         self.num_lives -= 1
                         print("Please, enter just one character")
                 else:    
@@ -57,6 +63,8 @@ class Hangman:
                             print(f'Hard luck! You guessed {letter} which is not a correct letter.') 
                             # print(f'you have {self.num_lives} lives left')
                             print(self.word_guessed)
+
+                # TODO This continue can be removed, it's redundant
                 continue
             else:
                 if self.num_lives<1:
@@ -70,13 +78,14 @@ class Hangman:
 
 def play_game(word_list):
     game = Hangman(word_list, num_lives=5)
+    # TODO As mentioned in line 39, the while loop should be here
+    # TODO Something like: 
+    # TODO while game.num_lives > 0 and '_' in game.word_guessed:
+    # TODO      game.ask_letter()
     game.ask_letter()
     pass
 
 if __name__ == '__main__':
     word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry', 'watermelon']
     play_game(word_list)
-# %%
-
-# %%
 
